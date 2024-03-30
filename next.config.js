9e9dde95fea4aa29ab683305347bc8ad10361179
DevGlauber <devglaubermonteiro@gmail.com>
2024-03-30 19:31:50 -0300

    removendo config de origin next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['https://agendamentos-8yoq.onrender.com'],
-    },
-  },
   logging: {
     fetches: {
       fullUrl: true,

12f088e7424bd244c4b4e71e8b3e24676215aaeb
DevGlauber <devglaubermonteiro@gmail.com>
2024-03-31 03:48:46 -0300

    removido componentes nao usados

--- src/components/layout/Sidebar/index.tsx
+++
@@ -1,9 +0,0 @@
-import SidebarCollapsible from './SidebarCollapsible'
-import SidebarRoot from './SidebarRoot'
-import SidebarTile from './SidebarTile'
-
-export const Sidebar = {
-  Root: SidebarRoot,
-  Tile: SidebarTile,
-  Collapsible: SidebarCollapsible,
-}

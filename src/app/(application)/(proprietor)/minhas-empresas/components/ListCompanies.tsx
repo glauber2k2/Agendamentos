20d8cf051cdf4c5571390e39b16bdd7b68c5e32d
DevGlauber <devglaubermonteiro@gmail.com>
2024-04-11 01:19:29 -0300

    adicioando filtragem por termo na tela de minhas-empresas

--- src/app/(application)/(proprietor)/minhas-empresas/components/ListCompanies.tsx
+++ src/app/(application)/(proprietor)/minhas-empresas/components/ListCompanies.tsx
@@ -6,6 +6,12 @@ import CardCompany from './CardCompany'
 import ModalAddEmpresa from './ModalAddEmpresa'
 import { Button } from '@/components/ui/button'
 import { Toggle } from '@/components/ui/toggle'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Input } from '@/components/ui/input'
 
 export interface Company {
   id: string
@@ -18,25 +24,46 @@ export interface Company {
 }
 
 export default function ListCompanies({ companies }: { companies: Company[] }) {
+  const [searchTerm, setSearchTerm] = useState('')
   const [isSorted, setIsSorted] = useState(false)
-  const [sortedCompanies, setSortedCompanies] = useState<Company[]>(companies)
 
   const orderCompanies = () => {
-    if (!isSorted) {
-      const sorted = [...companies].sort((a, b) => a.name.localeCompare(b.name))
-      setSortedCompanies(sorted)
-    } else {
-      setSortedCompanies(companies)
-    }
     setIsSorted(!isSorted)
+  }
+
+  const getFilteredAndSortedCompanies = () => {
+    const filteredCompanies = companies.filter(
+      (company) =>
+        company.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        company.business_name.toLowerCase().includes(searchTerm.toLowerCase()),
+    )
+
+    if (isSorted) {
+      filteredCompanies.sort((a, b) => a.name.localeCompare(b.name))
+    }
+
+    return filteredCompanies
   }
 
   return (
     <div className="flex flex-col gap-8">
       <div className="flex items-center justify-center gap-4">
-        <Button variant={'ghost'} size={'icon'}>
-          <Search />
-        </Button>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant={'ghost'} size={'icon'}>
+              <Search />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="border-none">
+            <Input
+              icon={Search}
+              placeholder="Buscar empresa..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </PopoverContent>
+        </Popover>
         <Toggle onClick={orderCompanies} variant={'ghost'} size={'icon'}>
           <ArrowUpDown />
         </Toggle>
@@ -44,7 +71,7 @@ export default function ListCompanies({ companies }: { companies: Company[] }) {
       </div>
 
       <div className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-4 w-full">
-        {sortedCompanies.map((company: Company) => (
+        {getFilteredAndSortedCompanies().map((company: Company) => (
           <CardCompany key={company.identifier} company={company} />
         ))}
       </div>
